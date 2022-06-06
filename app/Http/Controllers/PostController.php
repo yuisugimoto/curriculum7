8f414e7d0207beddf12f1eafeaf82d8157857e46
yuisugimoto <s.yui2525smile@ezweb.ne.jp>
2022-06-06 19:26:55 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,10 @@ class PostController extends Controller
 {
   public function index(Post $post)
    {
-      return $post->get();
+      return view('index')->with(['posts' => $post->getPaginateByLimit()]);
    }
+   public function show (Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@
 */
 
 Route::get('/posts', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
 
