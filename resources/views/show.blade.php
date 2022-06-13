b476c920c8e7e1d18201f2aa016859917289c92c
yuisugimoto <s.yui2525smile@ezweb.ne.jp>
2022-06-13 15:21:09 +0900

    カリキュラム8-4

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,6 +13,6 @@
             <p class='body'>{{ $post->body }}</p>
             <p class='updated_at'>{{ $post->updated_at }}</p>
         </div>
-        <div class='back'>[<a href='/'>back</a>]</div>
+        <div class='back'>[<a href='/posts'>back</a>]</div>
     </body>
 </html>
